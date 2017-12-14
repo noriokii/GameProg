45922be425623fa99eab65f6a897ed0d0d1f23eb
oki <noriokiv@gmail.com>
2017-12-14 12:44:16 +0700

    new commit

---
+++ Assets/DiceSide.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSide : MonoBehaviour {
+	public bool isSideDown = false;
+	public int value = 0;
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Terrain") {
+			isSideDown = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.tag == "Terrain") {
+			isSideDown = false;
+		}
+	}
+}
---
+++ Assets/RRoulete.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RRoulete : MonoBehaviour {
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		Rotate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.angularVelocity.z <= 0.1f) {
+			rb.angularVelocity = Vector3.zero;
+			Debug.Log (transform.rotation.eulerAngles.x);
+			//if(transform.rotation.eulerAngles.x <= 120 && transform.rotation.eulerAngles.x>=0){
+				
+
+			//}
+		}
+	}
+	void Rotate (){
+		rb.angularVelocity = new Vector3 (0f,0f,Random.Range(500f,1000f));
+
+	}
+}
